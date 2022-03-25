cea385c6dc51ee271b7ddda01c46713f8eef2b6c
sumeet siranje <94678048+Sumeet-Siranje@users.noreply.github.com>
2022-03-25 09:15:51 +0000

    p6original.c_done

--- p6original.c
+++ p6original.c
@@ -3,37 +3,27 @@ void input_string(char *a){
 printf("enter the string\n");
 scanf("%s",a);
 }
-int string_length(char *s){
-  int i;
-  for(i=0;s[i]!='\0';i++)
-    return i;  
-}
-char string_copy(char *d,char *s){
-  for(int i=0;s[i]!='\0';i++)
-    d[i]=s[i];
-  return *d;
-}
-char str_rev(char *s,char *t){
-  int length = string_length(s);
-  length-=1;
-  for(int i=0;i<length/2;i++){
-    t=s[i];
-    s[i]=s[length-i];
-    s[length-i]=t;
+int str_reverse(char *s){
+  int m=0;
+  for(int i=0;s[i]!='\0';i++){
+    m=m+1;
+    }
+  return m;
   }
-  return *t;
-}
-void output(char *s,char *r)
-{
-  printf("the reverse of %s is %s\n",s,r);
-}
+void output(char *a,char reverse){
+  printf("the reverse of the string is:\n");
+  for(int i=reverse-1;i>=0;i--)
+    printf("%c",a[i]);
+  }
 int main(){
-  char s[100];
-  char r[100];
-  input_string(s);
-  char original[100];
-  string_copy(original,s);
-  r==str_rev(s);
-  output(original,r);
+  char a[100];
+  int b;
+  input_string(a);
+ b = str_reverse(a);
+  output(a,b);
   return 0;
-  }
+  
+}
+  
+     
+
